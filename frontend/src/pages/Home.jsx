56fcf4c3c468783ead61d848e7cdcbee8d8d91a6
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-04-21 00:22:42 -0700

    added the ability to render the page based on certain states

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import AnimeCard from "../components/AnimeCard" 
+import {useState} from "react"
 
 function Home() {
+    // this part rerenders the state based on what the user types in the search
+    const [searchQuery, setSearchQuery] = useState("");
+
     const shows = [
         {id: 1, title: "One Piece", release_date: "1999"},
         {id: 1, title: "One Piece", release_date: "1999"},
@@ -8,12 +12,23 @@ function Home() {
     ]
 
     const handleSearch = () => {
-
+        // this prevents the refresh on submit
+        e.preventDefault()
+        alert(searchQuery)
     }
     
     return <div className = "home">
         <form onSubmit = {handleSearch} className = "search-form">
-            <input type="text" placeholder = "Search anime..." className = "search-input" />
+            <input 
+                type="text" 
+                placeholder = "Search anime..." 
+                className = "search-input" 
+                // connects the component to the search query
+                value = {searchQuery} 
+                // updating the state from an input element
+                onChange = {(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className = "search-button">Search</button>
         </form>
 
         <div className = "shows.grid">
